dad246c8a0caf564bcc4cef1d595ec5e3bb8899f
Julianne Vela <hello@juliannevela.dev>
2022-10-21 21:15:59 -0700

    gathered dom elements in app.js and commented initial steps

---
+++ form-challenge/app.js
@@ -0,0 +1,11 @@
+const fNameField = document.querySelector("#fName");
+const lNameField = document.querySelector("#lName");
+const emailField = document.querySelector("#email");
+const subCheckField = document.querySelector("#checkbox");
+const commentsField = document.querySelector("#comments");
+const submitButton = document.querySelector("#submit");
+
+// Disable submit button by default
+// Enabled once first and last name have at least 1 character in each field.
+// Hide input for email initially
+// If checkbox is checked, display input for email address.
