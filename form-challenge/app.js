de180245989f306732afc13bffeedc62cd48c058
Julianne Vela <hello@juliannevela.dev>
2022-10-21 21:45:46 -0700

    event listeners and subCheck added

--- form-challenge/app.js
+++ form-challenge/app.js
@@ -1,11 +1,31 @@
+const userDataForm = document.querySelector("#userData");
 const fNameField = document.querySelector("#fName");
 const lNameField = document.querySelector("#lName");
 const emailField = document.querySelector("#email");
+const emailFieldLabel = document.querySelector("#emailLabel");
 const subCheckField = document.querySelector("#checkbox");
 const commentsField = document.querySelector("#comments");
 const submitButton = document.querySelector("#submit");
 
 // Disable submit button by default
-// Enabled once first and last name have at least 1 character in each field.
+submitButton.disabled = true;
+
 // Hide input for email initially
+emailField.hidden = true;
+emailFieldLabel.hidden = true;
+
+// Submit enabled once first and last name have at least 1 character in each field.
+const nameComplete = { f: false, l: false };
+fNameField.addEventListener("change", (e) => {
+    if (e.target.textLength >= 1) nameComplete.f = true;
+});
+lNameField.addEventListener("change", (e) => {
+    if (e.target.textLength >= 1) nameComplete.l = true;
+});
+if (nameComplete.f && nameComplete.l) submitButton.disabled = false;
+
 // If checkbox is checked, display input for email address.
+if (subCheckField.checked) {
+    emailField.hidden = false;
+    emailFieldLabel.hidden = false;
+}
